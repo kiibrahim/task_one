8191aa6a48b497c893b2e59b1a6cce7dae779aa2
ibrahimmunna <ibrahimmunna059@gmail.com>
2022-08-17 19:21:45 +0200

    Client Side

--- client/src/components/Chart.vue
+++ client/src/components/Chart.vue
@@ -4,10 +4,10 @@
     <h2>Dashboard</h2>
     <div class="chart">
         <div class="chart1">
-         <canvas id="myChart" width="600" height="400"></canvas>
+            <canvas id="myChart" width="600" height="400"></canvas>
         </div>
         <div class="chart2">
-         <canvas id="myChart2" width="600" height="400"></canvas>
+            <canvas id="myChart2" width="600" height="400"></canvas>
         </div>
     </div>
 </div>
@@ -20,39 +20,39 @@ export default {
     mounted(){
         const ctx = document.getElementById('myChart');
         const ctx2 = document.getElementById('myChart2');
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-        datasets: [{
-            label: '# of sales',
-            data: [12, 19, 3, 5, 2, 3,5,3,7,1,9,4],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)','rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+        const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            datasets: [{
+                label: '# of sales',
+                data: [12, 19, 3, 5, 2, 3,5,3,7,1,9,4],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)','rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
             ],
             borderWidth: 1
         }]
@@ -65,27 +65,27 @@ const myChart = new Chart(ctx, {
         }
     }
 });
-const data = {
-  labels: [
-    'Chemical',
-    'Device',
-    'Service'
-  ],
-  datasets: [{
-    label: 'My First Dataset',
-    data: [300, 50, 100],
-    backgroundColor: [
-      'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 205, 86)'
-    ],
-    hoverOffset: 4
-  }]
+    const data = {
+        labels: [
+            'Chemical',
+            'Device',
+            'Service'
+        ],
+        datasets: [{
+        label: 'My First Dataset',
+        data: [300, 50, 100],
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+     }]
 };
-const myChart2 = new Chart(ctx2, {
-     type: 'doughnut',
-  data: data,
-});
+        const myChart2 = new Chart(ctx2, {
+            type: 'doughnut',
+            data: data,
+        });
     }
 }
 </script>
--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -2,22 +2,21 @@
     <div class="about">
        
         <div class="vision">
-        <h1>Our vision</h1>
-        <br>
-        <p>Automated, certified truck loading space cleaning should raise the cleanliness standards of logistics to a new level across the board and save resources such as water, energy and time.</p>
-        <br/>
+            <h1>Our vision</h1>
+            <br>
+            <p>Automated, certified truck loading space cleaning should raise the cleanliness standards of logistics to a new level across the board and save resources such as water, energy and time.</p>
+            <br/>
         </div>
-        <br/>
         <div class="culture">
-        <h1>Our culture</h1>
-        <br/>
-        <p>We are a young and innovative company from the heart of East Westphalia. From the founder to the working student, everyone is fully integrated into our family corporate culture!</p>
-        <br/>
+            <h1>Our culture</h1>
+            <br/>
+            <p>We are a young and innovative company from the heart of East Westphalia. From the founder to the working student, everyone is fully integrated into our family corporate culture!</p>
+            <br/>
         </div>
         <div class="history">
-        <h1>Our history</h1>
-        <br/>
-        <p>Again and again we have observed the time-consuming manual cleaning of the dirty cargo holds of livestock transporters in rural areas (Rietberg in North Rhine-Westphalia). Through our jobs in the manufacturing industry and the knowledge we acquired, we both came up with the idea of ​​developing an automated solution for this regular, inefficient, unpleasant, time-consuming work. In the further steps we found that truck trailers and sea containers are cleaned by hand with high-pressure cleaners worldwide. The protocols and guidelines of food logistics also require a certified cleaning of the holds.</p>
+            <h1>Our history</h1>
+            <br/>
+            <p>Again and again we have observed the time-consuming manual cleaning of the dirty cargo holds of livestock transporters in rural areas (Rietberg in North Rhine-Westphalia). Through our jobs in the manufacturing industry and the knowledge we acquired, we both came up with the idea of ​​developing an automated solution for this regular, inefficient, unpleasant, time-consuming work. In the further steps we found that truck trailers and sea containers are cleaned by hand with high-pressure cleaners worldwide. The protocols and guidelines of food logistics also require a certified cleaning of the holds.</p>
         </div>
     </div>
 </template>
